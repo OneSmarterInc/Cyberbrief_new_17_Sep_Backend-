1dfaaa57fad509dc52ddf3bb584b023705d3ac01
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-18 19:55:26 +0530

    Update apps.py

--- news_api/apps.py
+++ news_api/apps.py
@@ -1,13 +1,6 @@
 from django.apps import AppConfig
-import os
+
 
 class NewsApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_api'
-
-    def ready(self):
-        # == 'true' ensures the scheduler runs in the newly reloaded worker process!
-        if os.environ.get('RUN_MAIN') == 'true':
-            from . import scheduler
-            scheduler.start()
-            print("Background Email Scheduler Started!")
