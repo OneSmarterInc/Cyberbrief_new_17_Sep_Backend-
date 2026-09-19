cf525cb9f1970973d8b991d2c5c73989a8f0b2fe
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-19 14:55:45 +0530

    Change unsubscribe path to api/unsubscribe
    Updated unsubscribe path to include 'api/' prefix.

--- news_api/urls.py
+++ news_api/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     
     # --- NEWSLETTER & FEEDBACK ---
     path('subscribe/', views.subscribe_newsletter, name='subscribe_newsletter'),
-    path('unsubscribe/', views.unsubscribe_email, name='unsubscribe_email'),
     path('api/unsubscribe/', views.unsubscribe_email, name='unsubscribe_email'),
     path('audio/', views.generate_audio, name='generate_audio'),
     path("social/", views.get_social_links, name="get_social_links"),
