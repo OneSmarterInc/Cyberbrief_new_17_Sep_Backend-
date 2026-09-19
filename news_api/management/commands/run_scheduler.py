91b3deec2a9e53d32423edc21f97bb3b33effe41
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2026-09-19 15:10:49 +0530

    Start scheduler without blocking ingestion

--- news_api/management/commands/run_scheduler.py
+++ news_api/management/commands/run_scheduler.py
@@ -2,14 +2,13 @@ import time
 
 from django.core.management.base import BaseCommand
 
-from news_api.scheduler import scheduled_ingest, start
+from news_api.scheduler import start
 
 
 class Command(BaseCommand):
     help = "Run Cyberbrief background scheduler"
 
     def handle(self, *args, **options):
-        scheduled_ingest()
         start()
 
         self.stdout.write(self.style.SUCCESS("Cyberbrief scheduler started."))
